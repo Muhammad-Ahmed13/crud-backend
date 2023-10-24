5bda52dfaf0b7507cccb98867199c1d21a3df5b3
Muhammad-A <easytolearn7250@gmail.com>
2023-10-24 21:19:45 +0500

    new

--- router/auth.js
+++ router/auth.js
@@ -104,54 +104,28 @@ authRouter.get(GetUsers, async (req, res) => {
 //   }
 // });
 
-// authRouter.post(`${EditUser}/:id`, async (req, res) => {
-//   try {
-//     const { _id, ...updates } = req.body;
-//     const newUser = await User.findById(_id);
-
-//     if (!newUser) {
-//       return res.status(400).json({ msg: "User not found!" });
-//     }
-
-//     Object.entries(updates).forEach(([key, value]) => {
-//       if (value !== undefined) {
-//         newUser[key] = value;
-//       }
-//     });
-
-//     await newUser.save();
-//     res.json({ ...newUser._doc });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-
 authRouter.post(`${EditUser}/:id`, async (req, res) => {
   try {
-    const userId = req.params.id;
-    const updates = req.body;
+    const { _id, ...updates } = req.body;
+    const newUser = await User.findById(_id);
 
-    const user = await User.findById(userId);
-
-    if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+    if (!newUser) {
+      return res.status(400).json({ msg: "User not found!" });
     }
 
-    // Update user properties based on the keys present in the request body
-    for (const key in updates) {
-      if (updates.hasOwnProperty(key)) {
-        user[key] = updates[key];
+    Object.entries(updates).forEach(([key, value]) => {
+      if (value !== undefined) {
+        newUser[key] = value;
       }
-    }
+    });
 
-    await user.save();
-
-    res.json(user);
+    await newUser.save();
+    res.json({ ...newUser._doc });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
+
 
 
 authRouter.delete(`${DeleteUser}/:id`, async (req, res) => {
